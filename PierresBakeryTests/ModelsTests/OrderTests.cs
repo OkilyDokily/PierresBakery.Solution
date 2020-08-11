2c28809be0a1b5ef6dbacc08e693b178a028c3fe
me <me@me.me.com>
2020-08-11 13:30:50 -0700

    add a IDisposable class

--- PierresBakeryTests/ModelsTests/OrderTests.cs
+++ PierresBakeryTests/ModelsTests/OrderTests.cs
@@ -1,12 +1,18 @@
 using PierresBakery.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using System;
+
 
 namespace PierresBakeryTests.ModelsTests
 {
     [TestClass]
-    public class OrderTests
+    public class OrderTests : IDisposable
     {
+        public void Dispose(){
+            Order.Orders.Clear();
+        }
+
         [TestMethod] 
         public void AddToOrder_TestIfListHoldsOrder_True(){
             //Arrange
@@ -17,6 +23,17 @@ namespace PierresBakeryTests.ModelsTests
             Order.AddToOrder(new Pastry(7));
             //
             CollectionAssert.AreEqual(er, Order.Orders);
+        }
+        [TestMethod]
+          public void AddToOrder_TestIfListHoldsOrder_False(){
+            //Arrange
+            List<BakeryItem> er = new List<BakeryItem>(){new Bread(5), new Bread(6), new Pastry(7)};
+            //Act
+            Order.AddToOrder(new Pastry(5));
+            Order.AddToOrder(new Bread(6));
+            Order.AddToOrder(new Pastry(7));
+            //
+            CollectionAssert.AreNotEqual(er, Order.Orders);
         }
     }
 }
